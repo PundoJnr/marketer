60bf4a69d0a3a1a0a2896596321b306a97c86c87
PundoJnr <pundoarthur@gmail.com>
2020-01-20 09:20:21 +0300

    initial commit

---
+++ app/src/main/java/com/pundo/marketer/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pundo.marketer.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pundo.marketer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
